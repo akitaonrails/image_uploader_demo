a1c6f03beaf5baedfcda4cf19730f2d24ab57f97
vagrant <vagrant@precise64.(none)>
2014-03-22 10:11:49 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageUploadDemo::Application.config.secret_token = 'b19e5eafcc9bfd03838ad0d62411d6a541b0d4e61b3cc0ac018667c029365e0e82c75228bb9f8def6b59f6d43b12af59664e78b6e2cb63272a9cd0e009acc906'
